f7cb789704ba7d29b980cbf950f002192c9ddb33
Minedas Penkiolikmetis <vinckalc@gmail.com>
2025-05-21 16:51:32 +0300

    dank

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,7 +15,6 @@ const eslintConfig = [
     rules: {
       "react/no-unescaped-entities": "off",
       "@typescript-eslint/no-unused-vars":"off",
-      "@typescript-eslint/no-explicit-any":"off",
     },
   },
 ];
